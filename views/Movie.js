6efe12420a9863af6ec5e6271ea4621a88cd84d2
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-06-07 00:34:27 -0300

    feature: tela com dados sobre o filme reorganizada

--- views/Movie.js
+++ views/Movie.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { Button, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Button, Dimensions, Image, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 /**
- * @typedef {{ id: string, title: string, image: string}} FilmInfo
+ * @typedef {{ id: string, title: string, image: string, release_date: string}} FilmInfo
  */
 
+/**
+ * Tela que exibe informações de um filme
+ * 
+ * @param {object} param0 
+ * @param {{ params: FilmInfo }} param0.route
+ * @param {any} param0.navigation
+ */
 export const Movie = ({ route, navigation }) => {
+  const width = Dimensions.get('window').width * 0.85;
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.headerText}>Tela do filme específico {route.params.title}</Text>
+      <View style={styles.contentContainer}>
+        <Text style={styles.titleText}>{route.params.title}</Text>
+        <Text style={styles.subTitle}>({route.params.release_date})</Text>
+        <Image
+          style={styles.image}
+          source={{
+            width: width,
+            height: width * 1.50,
+            uri: route.params.image,
+          }}
+        />
         <Button title='Voltar' onPress={() => navigation.navigate('AllMovies')} />
       </View>
     </SafeAreaView>
@@ -25,16 +43,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  headerContainer: {
-    paddingTop: 16,
-    paddingBottom: 10,
+  contentContainer: {},
+  image: {
+    alignSelf: 'center',
+    marginTop: 4,
+    marginBottom: 10,
   },
-  headerText: {
+  titleText: {
     ...Platform.select({
       ios: { fontFamily: 'Arial', }, 
       android: { fontFamily: 'Roboto' }
     }),
+    textAlign: 'center',
     fontSize: 28,
-  }
+  },
+  subTitle: {
+    ...Platform.select({
+      ios: { fontFamily: 'Arial', }, 
+      android: { fontFamily: 'Roboto' }
+    }),
+    textAlign: 'center',
+    fontSize: 18,
+  },
 });
   
